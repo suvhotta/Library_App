aebb692034510da8867f540a265beb21b91e5231
Subhankar <suvhotta@gmail.com>
2020-04-21 17:03:20 +0530

    Login feature added and Bootstrap customized

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
 
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -14,7 +15,7 @@ class Books(db.Model):
         return f'<Book-title: {self.title} , ISBN:{self.isbn}>'
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     username = db.Column(db.String(10), nullable=False)
@@ -25,3 +26,8 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User: {self.username}, {self.email}>'
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -3,14 +3,23 @@ from app import app, bcrypt, db
 from app.forms import RegistrationForm, LoginForm
 from app.models import User
 from sqlalchemy import or_
+from flask_login import current_user, login_user, login_required, logout_user
+from datetime import timedelta
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/about',methods=['GET'])
+def about():
+    return render_template('about.html')
+
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     if(request.method == 'POST' and form.validate_on_submit()):
         #Hashing the password and decoding is part of syntax for python-3.
         hashed_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -25,13 +34,15 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if(request.method == "POST" and form.validate_on_submit()):
         user = User.query.filter(or_(User.username==form.login_field.data,
         User.email==form.login_field.data)).first()
         
         if(user and bcrypt.check_password_hash(user.password, form.password.data)):
-            flash('Login Successful', 'success')
+            login_user(user, remember=form.remember.data, duration=timedelta(seconds=5))
             return redirect(url_for('index'))
         
         else:
@@ -39,3 +50,10 @@ def login():
             return redirect(url_for('login'))
 
     return render_template('login.html', title='Login Page', form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
